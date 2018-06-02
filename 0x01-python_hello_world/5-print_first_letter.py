d2cfcbb2bcefe087d3149a14f1b7e89eaf63509d
Karla <liminal.karla@gmail.com>
2018-06-02 21:55:09 +0000

    more changes to 5 and 6

--- 0x01-python_hello_world/5-print_first_letter.py
+++ 0x01-python_hello_world/5-print_first_letter.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 my_word = "Holberton" 
-print("{} is the first letter of the word Holberton".format(my_word[0]))
+print("{} is the first letter of the word {}".format(my_word[0], my_word))
