3f1befa65e1dc894c34889a3860f733ee8aba6e5
Karla <liminal.karla@gmail.com>
2018-06-03 22:19:20 +0000

    comb3 improved

--- 0x02-python_if_else_loops_functions/100-print_comb3.py
+++ 0x02-python_if_else_loops_functions/100-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+print("{}".format('01'), end='')
 for i in range(0,10):
-  for j in range(0,10):
+  for j in range(2,10):
     if(j>i):
-      print("{}{}".format(i,j))
+      print(", {}{}".format(i,j),end='')
+print("{}".format(''))
